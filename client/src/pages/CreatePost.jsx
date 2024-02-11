624356a913e7efb7a7ae2d449b1c123c28c4c299
monar-22 <mona06547@gmail.com>
2024-02-11 16:26:06 +0530

    first commit

---
+++ client/src/pages/CreatePost.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const Container= styled.div`
+   height:100%;
+   overflow-y:scroll;
+   `;
+const CreatePost = () => {
+  return (
+    <Container>
+        createpost
+    </Container>
+      
+   
+  )
+}
+
+export default CreatePost
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,90 @@
+import React from 'react'
+import styled from 'styled-components'
+import SearchBar from '../components/SearchBar';
+import ImageCard from '../components/ImageCard';
+const Container= styled.div`
+   height:100%;
+   overflow-y:scroll;
+   padding: 30px 30px;
+   padding-bottom:50px;
+   display:flex;
+   flex-direction:column;
+   align-items:center;
+   gap: 20px;
+   @media (max-width: 768px){
+    padding: 6px 10px;
+   }
+   `;
+
+   const Headline = styled.div`
+   font-size: 30px;
+   font-weight:500;
+   color: ${({ theme }) => theme.text_primary};
+   display:flex;
+   align-items: center;
+   flex-direction:column;
+
+   @media (max-width: 700px){
+    font-size: 22px;
+   }
+   `;
+
+  const Span = styled.div`
+  font-size: 25px;
+   font-weight:800;
+   color: ${({ theme }) => theme.secondary};
+
+   @media (max-width: 600px){
+    font-size:20px;
+   }
+  `;
+
+  const Wrapper = styled.div`
+  width:100%;
+  max-width: 1400px;
+  padding:32px 0px;
+  display: flex;
+  justify-content:center;
+  `;
+
+  const CardWrapper = styled.div`
+  display: grid;
+  gap:20px;
+  @media(min-width:1200px){
+    grid-template-columns:repeat(4,1fr);
+  }
+  @media(min-width: 640px) and (max-width:1199px){
+    grid-template-columns:repeat(3,1fr);
+  }
+  @media(max-width:639px){
+    grid-template-columns:repeat(2,1fr);
+  }
+  `;
+const Home = () => {
+  const item = {
+    photo:"https://www.boredpanda.com/blog/wp-content/uploads/2023/05/317953525_1173916833514953_873670185157952690_n-64520e3eebdab__700.jpg",
+    author:"Riya ",
+    prompt:"  Tom riddleson childhood pic",
+}
+  return (
+    <Container>
+        <Headline>
+         Explore popular posts in the Community!
+        <Span>Generated with AI</Span>
+        </Headline>
+        <SearchBar />
+        <Wrapper>
+           <CardWrapper>
+              <ImageCard item={item}/>
+              <ImageCard item={item}/>
+              <ImageCard item={item}/>
+           </CardWrapper>
+
+        </Wrapper>
+    </Container>
+      
+    
+  )
+}
+
+export default Home
